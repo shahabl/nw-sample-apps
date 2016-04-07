1493f24bef325c9c2951538bc3cdafabbb79b516
Shahab Layeghi <shahab.layeghi@intel.com>
2016-04-06 18:20:48 -0700

    test uploading using http put

--- file-explorer/main.js
+++ file-explorer/main.js
@@ -52,6 +52,8 @@ var App = {
 $(document).ready(function() {
   var folder = new folder_view.Folder($('#files'));
   var addressbar = new abar.AddressBar($('#addressbar'));
+  var fs = require('fs');
+  //require('nw.gui').Window.get().showDevTools();
 
   folder.open(process.cwd());
   addressbar.set(process.cwd());
@@ -59,11 +61,47 @@ $(document).ready(function() {
   App.folder = folder;
   App.addressbar = addressbar;
 
+  function upload(file) {
+    var options = {
+      url: 'http://httpbin.org/put',
+      bodyType: 'binary',
+    };
+
+    var input = fs.readFileSync(file);
+    var oldProgress = 0;
+    var xhr = new XMLHttpRequest();
+    xhr.open('PUT', options.url, true);
+    
+    xhr.addEventListener('load', function () {
+      if (xhr.status >= 200 && xhr.status < 300 || xhr.status === 304) {
+        alert('upload done');
+      } else {
+        alert('Error: '+xhr.status);
+      }
+    }, false);
+    
+    xhr.addEventListener('error', function (e) {
+      alert('error: '+e.message);
+    }, false);
+    
+    xhr.upload.addEventListener('progress', function (p) {
+      var progress=0;
+      if(p.position && p.total) progress = p.position/p.total;
+      if(progress-oldProgress>0.2) {
+        alert('progress: '+progress);
+        oldProgress=progress;
+      }
+    }, false);
+    
+    xhr.send(input);
+    
+  }
+  
   folder.on('navigate', function(dir, mime) {
     if (mime.type == 'folder') {
       addressbar.enter(mime);
     } else {
-      gui.Shell.openItem(mime.path);
+      upload(mime.path);
     }
   });
 
